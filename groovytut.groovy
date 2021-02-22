948318d7ffac3af82eb2d0b94f26ff9900ab3ade
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-02-22 13:22:22 -0800

    groovy formatter

--- groovytut.groovy
+++ groovytut.groovy
@@ -1,5 +1,32 @@
-class GroovyTut{
-    static void main(String[] args){
-        
-    }
+class GroovyTut {
+
+       static void main(String[] args) {
+              println('Hello World')
+
+              def age = 'Dog'
+
+              println(age)
+
+              age = 40
+
+              println(age)
+
+              age += 1
+
+              println(age)
+
+              age += 0.1
+
+              println(age)
+
+              age += 0.1
+
+              println(age)
+
+              age = age.multiply(0.1)
+
+              println(age)
+
+       }
+
 }
