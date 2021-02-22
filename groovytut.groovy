d1cc65d7391a57e2e4b3f45e651e9d85d5300132
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-02-22 13:51:50 -0800

    practice

--- groovytut.groovy
+++ groovytut.groovy
@@ -4,28 +4,39 @@ class GroovyTut {
               println('Hello World')
 
               def age = 'Dog'
-
               println(age)
-
               age = 40
-
               println(age)
-
               age += 1
-
               println(age)
-
               age += 0.1
-
               println(age)
-
               age += 0.1
-
               println(age)
-
               age = age.multiply(0.1)
-
               println(age)
+
+              println(Integer.MAX_VALUE)
+              println(Integer.MIN_VALUE)
+              println(Long.MAX_VALUE)
+              println(Long.MIN_VALUE)
+              println(Float.MAX_VALUE)
+              println(Float.MIN_VALUE)
+
+              println(Double.NaN)
+
+              println('I am ${age}')
+              println("I am ${age}")
+              println('''
+              I am ${age}
+              ''')
+
+              def name = 'Freddie'
+
+              println("go by ${name[0..3]}")
+              for (c : name) {
+                     println("'${c}' : index = ${name.indexOf(c)}")
+              }
 
        }
 
